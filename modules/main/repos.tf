97743d39cac163e075fa4e5166917a832b1addbd
Michael Knopf <michael@herdkey.com>
2025-09-09 07:49:32 -0700

    Deployment envs (#3)

--- modules/main/repos.tf
+++ modules/main/repos.tf
@@ -1,20 +1,31 @@
+locals {
+  prod_reviewers = [
+    github_membership.michaelknopf.username,
+  ]
+}
+
 module "github-terraform" {
-  source      = "../repo"
-  name        = "github-terraform"
-  visibility  = "public"
-  description = "Terraform to manage this GitHub organization"
+  source         = "../repo"
+  name           = "github-terraform"
+  visibility     = "public"
+  description    = "Terraform to manage this GitHub organization"
+  envs           = ["play", "stage", "prod", "infra"]
+  prod_reviewers = local.prod_reviewers
 }
 
 module "infra-terraform" {
-  source      = "../repo"
-  name        = "infra-terraform"
-  visibility  = "private"
-  description = "Terraform to manage our AWS infrastructure"
+  source         = "../repo"
+  name           = "infra-terraform"
+  visibility     = "private"
+  description    = "Terraform to manage our AWS infrastructure"
+  envs           = ["play", "stage", "prod", "infra"]
+  prod_reviewers = local.prod_reviewers
 }
 
 module "hello-go" {
-  source      = "../repo"
-  name        = "hello-go"
-  visibility  = "public"
-  description = "Go backend server boilerplate"
+  source         = "../repo"
+  name           = "hello-go"
+  visibility     = "public"
+  description    = "Go backend server boilerplate"
+  prod_reviewers = local.prod_reviewers
 }
