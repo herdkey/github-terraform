c3cae8ac7326467dfd252066882795fda0e10d72
Michael Knopf <michael@herdkey.com>
2025-10-14 17:19:23 -0700

    onboard ryan

--- modules/live/members.tf
+++ modules/live/members.tf
@@ -3,6 +3,15 @@ resource "github_membership" "michaelknopf" {
   role     = "admin"
 }
 
+resource "github_membership" "ryan_coughlin" {
+  username = "coughlinslaws"
+  role     = "member"
+}
+
 output "michaelknopf" {
   value = github_membership.michaelknopf.id
+}
+
+output "ryan_coughlin" {
+  value = github_membership.ryan_coughlin.id
 }
